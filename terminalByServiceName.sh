b71d68b705630ad2eb0b7da49bf0a2256f963e5c
bbbowles <143605464+bbbowles@users.noreply.github.com>
2026-01-13 08:53:28 -0400

    Update terminalByServiceName.sh

--- terminalByServiceName.sh
+++ terminalByServiceName.sh
@@ -2,8 +2,8 @@
 
 SERVICE_NAME=$1
 
-NODEIP=$(docker node inspect "$(docker service ps $SERVICE_NAME --format '{{.Node}}')" --format '{{ .Status.Addr }}')
+NODEIP=$(docker node inspect "$(docker service ps $SERVICE_NAME --format '{{.Node}}' | head -n 1)" --format '{{ .Status.Addr }}')
 
-CONTAINERID=$(ssh "$NODEIP" "docker ps -aqf name=$SERVICE_NAME")
+CONTAINERID=$(ssh "$NODEIP" "docker ps -q --filter name=$SERVICE_NAME --filter status=running | head -n 1")
 
 ssh -t "$NODEIP" "docker exec -it $CONTAINERID bash"
